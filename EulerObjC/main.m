5cd5dcacc822e039b5051390c47d0fef0fb1fc58
James Hodgkinson <yaleman@ricetek.net>
2014-03-28 19:57:22 +1000

    Initial Commit

---
+++ EulerObjC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EulerObjC
+//
+//  Created by James Hodgkinson on 28/03/2014.
+//  Copyright (c) 2014 James Hodgkinson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
